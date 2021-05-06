93843a92ce714c379ab51fe7786bb20463f4ecf8
Renato Requena <36014971+Requena0177@users.noreply.github.com>
2021-05-05 23:23:56 -0600

    testeo k8s

--- frontend/src/app/@core/constantes.module.ts
+++ frontend/src/app/@core/constantes.module.ts
@@ -11,6 +11,10 @@ export const APPCONFIG:ConstantesConfig = {
   //BASE_URL_IMG: 'http://localhost:5000/'
 
   //TEST
-  BASE_URL: 'http://localhost:8080/api/',
-  BASE_URL_IMG: 'http://localhost:8080/'
+  BASE_URL: 'http://138.197.228.145:8080/api/',
+  BASE_URL_IMG: 'http://138.197.228.145:8080/'
+
+
+
+
 };
